14c091a0478956e1774f7bd0187db8468a942b5f
aniuka6 <ania.tokarzewska@wp.pl>
2023-02-04 19:46:58 +0100

    Dodawanie

--- src/Plane.java
+++ src/Plane.java
@@ -1,39 +1,37 @@
 public class Plane {
 
+//    private double MaxAircraftPayload;
+//    private int MaxAircraftSeat;
+
     private static final double MAX_AIRCRAFT_PAYLOAD = 800;
     private static final int MAX_AIRCRAFT_SEAT = 20;
     private String namePlane;
 
     private Passenger[] passengers = new Passenger[20];
-    private  int passengerNumber;
+    private int passengerNumber;
     private double onePassangerWeight;
     private double passengersWeight;
 
     private Luggage[] luggages = new Luggage[passengerNumber];
 
-    void addPassanger(Passenger passenger, Luggage luggage){
-         if ((passengerNumber < MAX_AIRCRAFT_SEAT) && ((passengersWeight + passenger.getWeight() + luggage.getLuggageWeight()) < MAX_AIRCRAFT_PAYLOAD) &&
-                 (passenger.getPesel().length() == 11)){
-                 passengers[passengerNumber] = passenger;
-                 passengerNumber ++;
-                 passengersWeight = (passengersWeight + passenger.getWeight() + luggage.getLuggageWeight());
-         }
+    void addPassanger(Passenger passenger, Luggage luggage) {
+        if ((passengerNumber < MAX_AIRCRAFT_SEAT) && ((passengersWeight + passenger.getWeight() + luggage.getLuggageWeight()) < MAX_AIRCRAFT_PAYLOAD) &&
+                (passenger.getPesel().length() == 11)) {
+            passengers[passengerNumber] = passenger;
+            System.out.println("Dodano pasażera");
+            passengerNumber++;
+            passengersWeight = (passengersWeight + passenger.getWeight() + luggage.getLuggageWeight());
+        } else {
+            System.out.println("nie dodano pasażera: " + passenger.getName());
 
-     }
-//
-//    void add(Passenger passenger){
-//        if ((passengerNumber < MAX_AIRCRAFT_SEAT) && ((passengersWeight + passenger.getWeight()) < MAX_AIRCRAFT_PAYLOAD)){
-//            passengers[passengerNumber] = passenger;
-//            passengerNumber ++;
-//            passengersWeight = (passengersWeight + passenger.getWeight());
-//        }
-
-    public Passenger[] getPassengers() {
-        Passenger[] pass = new Passenger[passengerNumber];
-        for (int i = 0; i < passengerNumber; i++) {
-            pass[i] = passengers[i];
-            ;
         }
-        return pass;
     }
-}
+        public Passenger[] getPassengers() {
+            Passenger[] pass = new Passenger[passengerNumber];
+            for (int i = 0; i < passengerNumber; i++) {
+                pass[i] = passengers[i];
+            }
+            return pass;
+        }
+    }
+
